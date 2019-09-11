c03db21eebeb0bea958d18ab87a23f10c456f657
STR <saturasstotenkopf@gmail.com>
2019-09-11 20:53:30 +0700

    Tugas 2 Javascript
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+var angka = 100;
+var string = "Hai";
+var bool = false;
+var undifined;
+var data = [1,2,3,4,5];
+
+console.log(angka);
+console.log(string);
+console.log(bool);
+console.log(undifined);
+console.log(data);
